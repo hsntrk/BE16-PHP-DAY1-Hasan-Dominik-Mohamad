5c1edd9e78fb1dea60f7d52471a5a59a047da123
MohamadHelli <mou.hamad123@hotmail.com>
2022-07-11 11:09:21 +0200

    basic

---
+++ Mohamad/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <?php
+    $name = "Mohamad Helli";
+    echo "<h1> Hello My name is $name.</h1>";
+
+
+
+    $fname = "Mohamad";
+    $age = 29;
+    $jobTitle = "IT";
+    echo "hi, my name is $fname, i am $age , and i work as a $jobTitle <br>";
+
+
+    $player = ["Mark", " John", "Georg" ,"Lisa"];
+    echo "the third player in the team is $player[2] ";
+    ?>
+</body>
+</html>
