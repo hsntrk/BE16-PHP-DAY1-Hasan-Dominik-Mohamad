31b943424bfca452e74ec30802bb95af56de040f
hsntrk <hsk@mailbox.org>
2022-07-11 11:10:26 +0200

    created exercise1

---
+++ Hasan/exercise1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    
+
+</head>
+<body>
+    <?php
+
+    $firstname = "HASAN";
+    $lastname = "ACARTÜRK";
+
+    echo " <h1> My name is $firstname $lastname </h1>";
+?>
+
+</body>
+</html>
+
+!
